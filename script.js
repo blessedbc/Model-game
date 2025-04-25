8a06ec32775f4a65a835feb2dccab4afd076a538
ikenna nwaka <inwaka39@gmail.com>
2025-04-25 08:36:46 +0200

    for loop

--- script.js
+++ script.js
@@ -5,3 +5,16 @@ const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
 const btnsOpenModal = document.querySelectorAll('.show-modal');
 console.log(btnsOpenModal);
+
+for (let i = 0; i < btnsOpenModal.length; i++)
+  btnsOpenModal[i].addEventListener('click', function () {
+ console.log('button clicked');
+ modal.classList.remove('hidden');
+ overlay.classList.remove('hidden');
+  });
+
+const closeModel = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
